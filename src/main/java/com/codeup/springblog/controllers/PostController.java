89871bf8f64d5359018a8812f9e610de1e4f8e74
viviancan <vcnales@gmail.com>
2021-02-10 16:47:07 -0600

    added post solutions

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,25 +1,41 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
     public String postsIndex(Model model){
+        Post post1 = new Post("First Post", "This is my first post", 1);
+        Post post2 = new Post("Second Post", "This is my 2nd post", 2);
+        Post post3 = new Post("Third Post", "This is my 3rd post", 3);
+
+        List<Post> postList = new ArrayList<>();
+        postList.add(post1);
+        postList.add(post2);
+        postList.add(post3);
 
         model.addAttribute("title", "All Posts");
+        model.addAttribute("posts", postList);
+
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
     public String postView(Model model){
 //        get single post by id later
+        Post post = new Post("First Post", "This is my first post", 1);
         model.addAttribute("title", "Single Posts");
+        model.addAttribute("post", post);
         return "posts/show";
     }
 
