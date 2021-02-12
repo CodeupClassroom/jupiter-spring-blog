a8aef950bf092a43829f3597b7de9cd775e5da5c
Sophia <sophia.sudiacal@gmail.com>
2021-02-12 14:18:43 -0600

    JPA exercise solutions

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -4,10 +4,7 @@ import com.codeup.springblog.models.Post;
 import com.codeup.springblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,23 +19,42 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public String postsIndex(Model model){
+    public String postsIndex(Model model) {
 
-
-        List<Post> postList = postsDao.findAll();
-
-
-        model.addAttribute("title", "All Posts");
-        model.addAttribute("posts", postList);
-
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String postView(Model model, @PathVariable long id){
+    public String postView(Model model, @PathVariable long id) {
 //        get single post by id later
         Post post = postsDao.getOne(id);
+        model.addAttribute("post", post);
         return "posts/show";
+    }
+
+    @GetMapping("/posts/{id}/edit")
+    public String viewEditPostForm(@PathVariable long id, Model model) {
+        model.addAttribute("post", postsDao.getOne(id));
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/{id}/edit")
+    public String updatePost(@PathVariable long id, @RequestParam String title, @RequestParam String body) {
+        Post post = new Post(
+                id,
+                title,
+                body
+        );
+        postsDao.save(post);
+        return "redirect:/posts";
+    }
+
+    @PostMapping("/posts/{id}/delete")
+    public String deletePost(@PathVariable long id){
+        System.out.println("Deleting post...");
+        postsDao.deleteById(id);
+        return "redirect:/posts";
     }
 
     @GetMapping("/posts/create")
